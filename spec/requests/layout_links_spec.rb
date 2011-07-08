cdad33e4e311593c4ff3a6c3f1047d60b0c06b91
Bodaniel Jeanes <me@bjeanes.com>
2011-07-08 13:42:51 -0500

    factored out the integration_sign-in

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -48,11 +48,7 @@ describe "LayoutLinks" do
   end
   describe "when signed in" do
     before :each do
-      @user = Factory(:user)
-      visit signin_path
-      fill_in :email,  :with => @user.email
-      fill_in :password,  :with => @user.password
-      click_button
+      integration_test_sign_in
     end
     it "should have a signout link" do
       visit root_path
