037e09fc921d05dd42507efbdc9543a57110ce7c
Bodaniel Jeanes <me@bjeanes.com>
2011-07-12 13:53:27 -0500

    smart redirecting

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -26,13 +26,28 @@ module SessionsHelper
   end
 
   def deny_access
+    store_location
     redirect_to signin_path, :notice => "Please sign in to access this page"
   end
+
+  def redirect_back_or(default)
+    redirect_to(session[:redirect_to] || default)
+    clear_return_to
+  end
+
   def user_from_remember_token
     User.authenticate_with_salt(*remember_token)
   end
 
   def remember_token
     cookies.signed[:remember_token] || [nil, nil]
+  end
+
+  def store_location
+    session[:redirect_to] = request.fullpath
+  end
+
+  def clear_return_to
+    session[:redirect_to] = nil
   end
 end
---
+++ spec/requests/friendly_forwardings_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe "FriendlyForwardings" do
+  it "should redirect to the requested page after signin" do
+    user = Factory(:user)
+    visit edit_user_path(user)
+    #redirect to sign-in
+    fill_in :email, :with => user.email
+    fill_in :password, :with => user.password
+    click_button
+    response.should render_template('users/edit')
+  end
+end
