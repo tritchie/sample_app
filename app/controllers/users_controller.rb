7f0a058c41e9e44b607e5c9c326bbf979aa5400b
Bodaniel Jeanes <me@bjeanes.com>
2011-07-12 11:03:52 -0500

    fully protected

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_filter :authenticate, :only => [ :edit, :update ]
+  before_filter :correct_user, :only => [ :edit, :update ]
 
   def show
     @user = User.find(params[:id])
@@ -27,7 +28,6 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
     @title = 'Edit user'
     @button_name = 'Update'
   end
@@ -46,5 +46,9 @@ class UsersController < ApplicationController
   private
   def authenticate
     deny_access unless signed_in?
+  end
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -205,5 +205,19 @@ describe UsersController do
         response.should redirect_to signin_path
       end
     end
+    describe "For signed-in users" do
+      before(:each) do
+        wrong_user = Factory(:user, :email => 'user@example.net')
+        test_sign_in(wrong_user)
+      end
+      it "should require matching users for 'edit'" do
+        get :edit, :id => @user
+        response.should redirect_to root_path
+      end
+      it "should require matching users for 'update'" do
+        get :edit, :id => @user, :user => {}
+        response.should redirect_to root_path
+      end
+    end
   end
 end
